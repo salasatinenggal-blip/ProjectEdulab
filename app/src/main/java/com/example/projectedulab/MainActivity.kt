7bea2c67fc4278ec8d84e25298b54c68830fa964
salasatin <salasatinenggal@gmail.com>
2025-11-29 17:53:24 +0700

    Errror semuaaaaaaaaaaaa

--- app/src/main/java/com/example/projectedulab/MainActivity.kt
+++ app/src/main/java/com/example/projectedulab/MainActivity.kt
@@ -1,26 +1,39 @@
 package com.example.projectedulab
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.projectedulab.HomeFragment
+import com.example.projectedulab.TemplateFragment
+import com.example.projectedulab.HistoryFragment
+import com.example.projectedulab.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment, HomeFragment())
+            .commit()
+
+        bottomNav.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> { loadFragment(HomeFragment()); true }
+                R.id.nav_template -> { loadFragment(TemplateFragment()); true }
+                R.id.nav_history -> { loadFragment(HistoryFragment()); true }
+                R.id.nav_profile -> { loadFragment(ProfileFragment()); true }
+                else -> false
+            }
         }
     }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment, fragment)
+            .commit()
+    }
 }
-
-
-//saya naya
-// jsjsjsjj
-//tai
-//eeg
