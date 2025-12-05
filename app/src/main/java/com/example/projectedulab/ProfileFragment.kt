9d005b87ba637f8e34db87ab7b21c1cf7483877d
fransiskus paling enjoy <frengkipasut@gmail.com>
2025-12-05 22:17:35 +0700

    profil e ya lek enjoy mugo iso

--- app/src/main/java/com/example/projectedulab/ProfileFragment.kt
+++ app/src/main/java/com/example/projectedulab/ProfileFragment.kt
@@ -1,11 +1,14 @@
 package com.example.projectedulab
-// Ganti dengan nama paket (package) proyek Anda
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.fragment.app.Fragment
 
 class ProfileFragment : Fragment() {
 
@@ -13,12 +16,24 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Menginflate (memuat) layout XML: fragment_profile.xml
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Logika spesifik ProfileFragment dapat ditambahkan di sini
+
+        val imgProfile = view.findViewById<ImageView>(R.id.imgProfile)
+        val edtNama = view.findViewById<EditText>(R.id.edtNama)
+        val edtEmail = view.findViewById<EditText>(R.id.edtEmail)
+        val edtPhone = view.findViewById<EditText>(R.id.edtPhone)
+        val radioGender = view.findViewById<RadioGroup>(R.id.radioGender)
+        val rbLaki = view.findViewById<RadioButton>(R.id.rbLaki)
+        val rbPerempuan = view.findViewById<RadioButton>(R.id.rbPerempuan)
+
+        // Isi contoh data
+        edtNama.setText("User Edulab")
+        edtEmail.setText("user@example.com")
+        edtPhone.setText("08123456789")
+        rbLaki.isChecked = true
     }
 }
