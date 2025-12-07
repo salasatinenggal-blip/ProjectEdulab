0036c4ed395d16db239589d0a2ab143c22845434
salasatin <salasatinenggal@gmail.com>
2025-12-07 13:51:53 +0700

    detail template

--- app/src/main/java/com/example/projectedulab/DetailTemplateActivity.kt
+++ app/src/main/java/com/example/projectedulab/DetailTemplateActivity.kt
@@ -7,7 +7,6 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
-
 class TemplateDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
